aaa00db70dada6840876ffca443f2a30944005b1
tzsu <tzu-lin.su@helsinki.fi>
2017-02-10 23:02:31 +0200

    exercise 3

--- data/create_alc.R
+++ data/create_alc.R
@@ -4,9 +4,11 @@
 #Data source: UCI Machine Learning Reposiory - Student Alcohol Consumption 
 #P. Cortez and A. Silva. Using Data Mining to Predict Secondary School Student Performance. In A. Brito and J. Teixeira Eds., Proceedings of 5th FUture BUsiness TEChnology Conference (FUBUTEC 2008) pp. 5-12, Porto, Portugal, April, 2008, EUROSIS, ISBN 978-9077381-39-7.
 
-#set workig directory
+#set working directory
 setwd("D:/Sydney Uni/forth semester units of study/Intro to Open Data Science/IODS-project/data")
 
+#access the dplyr library
+library(dplyr)
 
 #Read student-mat and student-por into R
 #Explore the structure and dimensions of the data
@@ -14,9 +16,6 @@ math <- read.table("student-mat.csv", sep = ';', header = TRUE)
 por <- read.table("student-por.csv", sep = ';', header = TRUE)
 glimpse(math)
 glimpse(por)
-
-#access the dplyr library
-library(dplyr)
 
 #Join the two data sets using the common columns to use as identifiers
 join_by <- c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
@@ -76,11 +75,56 @@ alc <- mutate(alc, high_use = alc_use > 2)
 glimpse(alc)
 
 #save the new dataset in the 'data' folder as 'alc.txt'
-write.table(alc, "alc.txt", row.names = FALSE)
+write.table(alc, file = "alc.csv", sep = "\t", col.names = TRUE)
+#end of data wrangling part
+
+alc <- read.table("alc.csv", sep ="\t", header = TRUE)
+
+#Taking a glimpse at the combined dataset
+glimpse(alc)
+
+library(ggplot2)
+
+# initialize a plot of high_use and G3
+g1 <- ggplot(alc, aes(x = high_use, y = G3, col=sex)) + geom_boxplot() + ylab('final grade') + xlab('high_use')
+# define the plot as a boxplot and draw it
+g1 + geom_boxplot() + ylab("grade")+ ggtitle("Student final grades by alcohol consumption and sex")
+
+# initialise a plot of high_use and absences
+g2 <- ggplot(alc, aes(x= high_use, y = absences, col = sex))
+# define the plot as a boxplot and draw it
+g2 +geom_boxplot() + ggtitle("Student absences by alcohol consumption and sex")
+
+#initialise a plot of high_use and freetime
+g4 <- ggplot(alc, aes(freetime)) + geom_bar(aes(fill = high_use), position = "dodge", stat="count") 
+#define the plot as a barplot and draw it
+g4  + ggtitle("Student freetime by alcohol consumption")
+
+#initialise a plot of high_use and romantic
+g5 <- ggplot(alc, aes(romantic)) + geom_bar(aes(fill = high_use), position = "dodge", stat="count") 
+#define the plot as a barplot and draw it
+g5  + ggtitle("Student romantic by alcohol consumption")
 
 
+# find the model with glm()
+m <- glm(high_use ~ G3 + absences + freetime + romantic, data = alc, family = "binomial")
+# print out a summary of the model
+summary(m)
+# compute odds ratios (OR)
+OR <- coef(m) %>%exp
+#compute confidence intervals(CI)
+CI <- exp(confint(m))
 
+# find the new model with glm()
+m <- glm(high_use ~ absences + freetime, data = alc, family = "binomial")
+# print out a summary of the model
+summary(m)
 
-
-
-
+# predict() the probability of high_use
+probabilities <- predict(m, type = "response")
+# add the predicted probabilities to 'alc'
+alc <- mutate(alc, probability = probabilities)
+# use the probabilities to make a prediction of high_use
+alc <- mutate(alc, prediction = probability>0.5)
+# tabulate the target variable versus the predictions
+table(high_use = alc$high_use, prediction = alc$prediction)
